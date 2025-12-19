a50c68076ef3e58b87ef9e689dcf350c2eebee79
MO <57251099+MAbdulfattah@users.noreply.github.com>
2025-12-19 13:48:05 +0100

    First steps to mastering SQL

---
+++ SelectTutorials.sql
@@ -0,0 +1,15 @@
+SELECT * 
+FROM employee_demographics;
+
+SELECT first_name,
+last_name,
+birth_date,
+age, 
+(age + 10) * 10
+FROM employee_demographics;
+
+SELECT DISTINCT first_name, gender 
+FROM employee_demographics;
+
+SELECT distinct gender 
+FROM employee_demographics;
